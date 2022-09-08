dcebba340f0b096258ebd926088c2521ac7d5b01
Lindsey <lindseykaycarlson@outlook.com>
2022-09-08 14:20:58 -0700

    Feature 4 completed & tested - registered models in app admin

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import ExpenseCategory, Account, Receipt
 # Register your models here.
+
+admin.site.register(ExpenseCategory)
+admin.site.register(Account)
+admin.site.register(Receipt)
