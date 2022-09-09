3fdf387667d6ecefa774e14f9fca0ebea8916c3e
Lindsey <lindseykaycarlson@outlook.com>
2022-09-09 14:50:01 -0700

    Feature 11 completed/tested - create view

--- receipts/views.py
+++ receipts/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from receipts.models import Receipt
 
 # Create your views here.
 
+
 class ReceiptListView(LoginRequiredMixin, ListView):
     model = Receipt
     template_name = "receipts/list.html"
-    #context_object_name="receipt"
+    # context_object_name="receipt"
 
     def get_queryset(self):
         return Receipt.objects.filter(purchaser=self.request.user)
+
+
+class ReceiptCreateView(LoginRequiredMixin, CreateView):
+    model = Receipt
+    template_name = "receipts/new.html"
+    fields = ["vendor", "total", "tax", "date", "category", "account"]
+
+    def form_valid(self, form):
+        new_receipt = form.save(commit=False)
+        new_receipt.purchaser = self.request.user
+        new_receipt.save()
+        return redirect("home")
